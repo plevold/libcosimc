7412f2fce03452cc3cf939bb435c469b629f2a5d
Pål Levold <paal@levold.net>
2025-01-02 12:48:10 +0100

    work in progress

--- conanfile.py
+++ conanfile.py
@@ -1,7 +1,7 @@
 import os
 
-from conans import ConanFile, CMake, tools
-from os import path
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, CMakeDeps, cmake_layout
 
 
 class LibCosimCConan(ConanFile):
@@ -14,34 +14,44 @@ class LibCosimCConan(ConanFile):
         "revision": "auto"
     }
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake", "virtualrunenv"
-    requires = (
-        "libcosim/0.10.2@osp/stable"
-        )
 
-    def set_version(self):
-        self.version = tools.load(path.join(self.recipe_folder, "version.txt")).strip()
+    def requirements(self):
+        self.tool_requires("cmake/[>=3.19]")
+        self.requires("libcosim/0.10.4@osp/stable")
 
-    def imports(self):
-        binDir = os.path.join("output", str(self.settings.build_type).lower(), "bin")
-        self.copy("*.dll", dst=binDir, keep_path=False)
-        self.copy("*.pdb", dst=binDir, keep_path=False)
+    def layout(self):
+        cmake_layout(self)
 
-    def configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["LIBCOSIMC_USING_CONAN"] = "ON"
-        cmake.configure()
-        return cmake
+    def generate(self):
+        # TODO: Remove?
+        # Copy dependencies to the folder where executables (tests, mainly)
+        # will be placed, so it's easier to run them.
+        # bindir = os.path.join(
+        #     self.build_folder,
+        #     "output",
+        #     str(self.settings.build_type).lower(),
+        #     "bin")
+        # for dep in self.dependencies.values():
+        #     for depdir in dep.cpp_info.bindirs:
+        #         copy(self, "*.dll", depdir, bindir, keep_path=False)
+        #         copy(self, "*.pdb", depdir, bindir, keep_path=False)
+        #         copy(self, "proxyfmu*", depdir, bindir, keep_path=False)
+
+        # Generate CMake toolchain file
+        tc = CMakeToolchain(self)
+        tc.cache_variables["LIBCOSIMC_USING_CONAN"] = "ON"
+        tc.generate()
+        CMakeDeps(self).generate()
 
     def build(self):
-        cmake = self.configure_cmake()
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
         cmake.build(target="doc")
 
     def package(self):
-        cmake = self.configure_cmake()
+        cmake = CMake(self)
         cmake.install()
-        cmake.build(target="install-doc")
 
     def package_info(self):
-        self.cpp_info.libs = [ "cosimc" ]
+        self.cpp_info.libs = ["cosimc"]
